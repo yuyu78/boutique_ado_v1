ea474aac87e0d8568c222418ecb45eb216452383
yuyu78 <ishiiyuki78@gmail.com>
2021-10-06 17:35:56 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Jhd8RBWfcC3iv59lgWJMphNlzBhLTrIkoLIs8NP4qbDtfMiZV1hHpO8JV4ItiF9IokUrnFCyAEJxTJw7C0loxAN00U9PZJxh6',
+        'client_secret': 'text client secret', 
     }
 
     return render(request, template, context)
